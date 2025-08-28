962d8f5a2e70752b1f0e885ff026e4bff9dc261b
Rigby <rigby.2593046@nv.ccsd.net>
2025-08-28 08:15:49 -0700

    Update CPP-Number-Game.cpp

--- CPP-Number-Game.cpp
+++ CPP-Number-Game.cpp
@@ -2,30 +2,33 @@
 using namespace std;
 
 int main() {
-    srand(time(0));
-    int num = (rand() % 101);
-    int guess;
-    int attemps = 0;
+    srand(time(0)); // Initialize random number generator 
     
-    cout << "welcome to the number guessing game (enter a number between 1-100)\n";
+    int num = (rand() % 101), attemps = 0, guess; // Declare vars
+    
+    cout << "Welcome to the Gumber Game!" << endl; // Switched from using '\n' to endl
     
     while (true) {
-        cout << "enter a number: ";
+        cout << "Please enter a number: ";
         cin >> guess;
+        
+        if (attemps > 10) {
+            cout << "Sorry, but you lost, due to too many attempts!"; // This doesn't need an 'endl', because it's the last string being printed in the program.
+            break;
+        }
         
         if (guess == num) {
             attemps++;
+            cout << "Congrats! You've guessed the number in " << attemps << " attemps!"; // This doesn't need an 'endl', because it's the last string being printed in the program.
             break;
         } else if (guess > num) {
-            cout << "the number is too high, try again.\n";
+            cout << "Too high, try again." << endl; // Switched from using '\n' to endl
             attemps++;
-        }
-        else if (guess < num) {
-            cout << "the number is too low, try again.\n";
+        } else if (guess < num) {
+            cout << "Too low, try again." << endl; // Switched from using '\n' to endl
             attemps++;
         }
     }
-    
-    cout << "congrats you've guessed the number!\n";
+
     return 0;
 }
